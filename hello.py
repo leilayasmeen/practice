ed9565705f46d639a1ac5adbbaac1a07c06e3d8e
Leila Islam Ara <leilayasmeenara@gmail.com>
2018-06-01 15:30:14 +0100

    added os line

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 hello = tf.constant('Hello, TensorFlow!')
 gpu_config = tf.ConfigProto()
 gpu_config.gpu_options.visible_device_list = str(2)
